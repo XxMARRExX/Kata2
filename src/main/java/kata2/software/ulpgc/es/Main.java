5569ab26997406edf1e2e3bb478351465235dc91
XxMARRExX <xx_marre_xx@hotmail.com>
2024-06-27 10:43:04 +0100

    Implementation of Main

--- src/main/java/kata2/software/ulpgc/es/Main.java
+++ src/main/java/kata2/software/ulpgc/es/Main.java
@@ -2,5 +2,25 @@ package kata2.software.ulpgc.es;
 
 import model.kata2.software.ulpgc.es.*;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
+
+    public static void main(String[] args) {
+        List<F1DriverRegister> registers = CSVFileLoader
+                .with(new File("drivers_updated.csv"))
+                .load();
+        TeamPointsStatistic statistic = new TeamPointsStatistic();
+        Map<String, Float> map = statistic.calculate(registers);
+
+        int count = 0;
+        for (String key: map.keySet()) {
+            if(count >= 10) break;
+            System.out.println(key + " = " + map.get(key));
+            count++;
+        }
+    }
+
 }
